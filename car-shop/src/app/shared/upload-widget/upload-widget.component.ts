02159107c94f4385e8ae9d535f6ff819def4dfa1
Slavi Hristev <Slavihristev97@gmail.com>
2024-11-26 17:34:18 +0200

    Fixed an issue with the upload widget and now we can upload images to claudinary and display them

--- car-shop/src/app/shared/upload-widget/upload-widget.component.ts
+++ car-shop/src/app/shared/upload-widget/upload-widget.component.ts
@@ -1,64 +1,74 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+  OnInit,
+  AfterViewInit
+} from '@angular/core';
 import { CloudinaryScriptService } from '../../services/cloudinary.service';
 
 @Component({
   selector: 'app-upload-widget',
   templateUrl: './upload-widget.component.html',
 })
-export class UploadWidgetComponent implements OnInit {
-  @ViewChild('uploadButton', { static: true })
+export class UploadWidgetComponent implements OnInit, AfterViewInit {
+  @ViewChild('uploadButton', { static: false }) 
   uploadButton!: ElementRef<HTMLButtonElement>;
 
-  loaded = false;
-  publicState: string[] = []; 
-  @Output() imagesUploaded = new EventEmitter<string[]>(); 
+  loaded = false; 
+  publicState: string[] = [];
+
+  @Output() imagesUploaded = new EventEmitter<string[]>();
 
   constructor(private cloudinaryService: CloudinaryScriptService) {}
 
   ngOnInit() {
+    const scriptId = 'cloudinary-widget-script';
 
-    this.loaded = document.getElementById('uw')?.isConnected ?? false; 
-    if (!this.loaded) {
+
+    if (!document.getElementById(scriptId)) {
       const script = document.createElement('script');
-      script.setAttribute('async', '');
-      script.setAttribute('id', 'uw');
+      script.id = scriptId;
+      script.async = true;
       script.src = 'https://upload-widget.cloudinary.com/global/all.js';
-      script.onload = () => this.loaded = true;
+      script.onload = () => {
+        this.loaded = true; 
+        this.initializeCloudinaryWidget();
+      };
       document.body.appendChild(script);
+    } else {
+      this.loaded = true;
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.loaded) {
+      this.initializeCloudinaryWidget();
     }
   }
 
   initializeCloudinaryWidget() {
+ 
     if (this.loaded && this.cloudinaryService.uwConfig && window.cloudinary) {
       const myWidget = window.cloudinary.createUploadWidget(
         this.cloudinaryService.uwConfig,
-        (error: any, result: { event: string; info: { secure_url: string; }; }) => {
-          if (!error && result && result.event === 'success') {
-            console.log('Done! Here is the image info: ', result.info);
-            this.publicState = [...this.publicState, result.info.secure_url];
+        (error: any, result: { event: string; info: { secure_url: string } }) => {
+          if (!error && result.event === 'success') {
+            console.log('Uploaded image info:', result.info);
+            this.publicState.push(result.info.secure_url);
             this.imagesUploaded.emit(this.publicState);
           }
         }
       );
 
-      this.uploadButton.nativeElement.addEventListener('click', () => myWidget.open());
-    }
-  }
-
-  
-  uploadImages(): void {
-    if (this.loaded && window.cloudinary) {
-      const myWidget = window.cloudinary.createUploadWidget(
-        this.cloudinaryService.uwConfig,
-        (error: any, result: { event: string; info: { secure_url: string; }; }) => {
-          if (!error && result && result.event === 'success') {
-            console.log('Done! Here is the image info: ', result.info);
-            this.publicState = [...this.publicState, result.info.secure_url];
-            this.imagesUploaded.emit(this.publicState); 
-          }
-        }
-      );
-      myWidget.open();
+      
+      if (this.uploadButton) {
+        this.uploadButton.nativeElement.addEventListener('click', () => myWidget.open());
+      } else {
+        console.error('Upload button not initialized.');
+      }
     }
   }
 }
